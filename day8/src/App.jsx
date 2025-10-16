6b43efdf2ee1166c612217ed8c20e660b48e5638
adityakumar70426 <adityakumar70426@gmail.com>
2025-10-16 10:38:09 +0530

    day8

--- day8/src/App.jsx
+++ day8/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import UserInput from './UserInput'
 // import './App.css'
+import {useFormStatus} from 'react-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,24 @@ function App() {
     inputRef.current.focus();
     inputRef.current.style.color="red";
   }
+  const handlesubmit=async()=>{
+    await new Promise(res=>setTimeout(res,2000));
+    console.log("submitted");
+  }
+
+  function CustomerForm(){
+    const {pending}=useFormStatus();
+    console.log(pending);
+    return(
+      <div>
+        <input type="text" placeholder='Enter Name' />
+        <br />
+        <input type="text" placeholder='Enter password' />
+        <br />
+        <button disabled={pending}>{pending?'Submitting...':'Submit'}</button>
+      </div>
+    )
+  }
 
   return (
     <div>
@@ -20,8 +39,60 @@ function App() {
       <UserInput ref={inputRef}/>
       <button onClick={updateInput}>Update</button>
 
+
+      <br /><br />
+      <hr />
+      
+
+      <h2>useFormStatus hook</h2>
+      <form action={handlesubmit}>
+       <CustomerForm />
+      </form>
+
     </div>
   )
 }
 
 export default App
+
+
+
+// import {useFormStatus} from 'react-dom'
+// function App() {
+
+
+//   const handleSubmit=async ()=>{
+//     await new Promise(res=>setTimeout(res,5000));
+//     console.log("submit");
+    
+//   }
+
+//   function CustomerForm(){
+//     const {pending,data,method} =useFormStatus();
+//     console.log(pending,data,method);
+    
+//     return(
+//       <div>
+//         <input type="text" placeholder="Enter Name" />
+//       <br />
+//       <br />
+//       <input type="text" placeholder="Enter Password" />
+//       <br />
+//       <br />
+//       <button disabled={pending}>{pending?'Submitting...':'Submit'}</button>
+
+//       </div>
+//     )
+//   }
+//   return (
+//    <div>
+//     <h1>useFormStatus Hook in React js 19</h1>
+//     <form action={handleSubmit} method='post'>
+//       <CustomerForm />
+//     </form>
+//    </div>
+
+//   );
+// }
+
+// export default App;
